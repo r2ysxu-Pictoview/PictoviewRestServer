e8197bcfd7535cf20aecf90b6674b0440b14c96b
Arthur Xu <r2ysxu@gmail.com>
2015-04-09 14:03:16 -0400

    Made bean member variable and added get photo data

--- src/com/viewer/rest/AlbumMessageHandler.java
+++ src/com/viewer/rest/AlbumMessageHandler.java
@@ -9,28 +9,65 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.codec.binary.Base64;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.viewer.beans.AlbumBeanLocal;
-import com.viewer.dto.PhotoDTO;
+import com.viewer.dto.AlbumDTO;
 
 @Path("/viewer")
 public class AlbumMessageHandler {
 
-	@GET
-	@Path("/album")
-	public Response albumInfo() {
+	AlbumBeanLocal albumRemote;
 
+	public AlbumMessageHandler() {
 		try {
-
 			Context c = new InitialContext();
-			AlbumBeanLocal albumRemote = (AlbumBeanLocal) c
+			albumRemote = (AlbumBeanLocal) c
 					.lookup("java:global/PictureViewerEAR/PictureViewerEJB/AlbumBean!com.viewer.beans.AlbumBeanLocal");
-
-			List<PhotoDTO> albums = albumRemote.fetchUserAlbumPhotos(1, 1);
-			System.out.println(albums.get(0).getName());
 		} catch (NamingException e1) {
 			e1.printStackTrace();
 		}
-		String output = "hello";
+	}
+
+	@GET
+	@Path("/album")
+	public Response getUserAlbums() {
+
+		List<AlbumDTO> albums = albumRemote.fetchAllUserAlbums(1);
+		String output = generateAlbumJSON(albums);
 		return Response.status(200).entity(output).build();
+	}
+
+	@GET
+	@Path("/image")
+	public Response getUserAlbumPhoto() {
+		byte[] imageByteArray = albumRemote.fetchPhotoData(1);
+		String output = Base64.encodeBase64URLSafeString(imageByteArray);
+		return Response.status(200).entity(output).build();
+	}
+
+	/**
+	 * Turns a collection of AlbumDTOs to JSON string
+	 * 
+	 * @param albums
+	 * @return JSON string
+	 */
+	private String generateAlbumJSON(List<AlbumDTO> albums) {
+		JSONArray albumsJSON = new JSONArray();
+		try {
+			for (AlbumDTO album : albums) {
+				JSONObject albumJSON = new JSONObject();
+				albumJSON.put("id", album.getId());
+				albumJSON.put("name", album.getName());
+				albumJSON.put("coverId", album.getCoverId());
+				albumsJSON.put(albumJSON);
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return albumsJSON.toString();
 	}
 }
